83890273bad307991c79b96f28963d45b6bdef47
Ice-Three <bjorkholmoskar@gmail.com>
2024-01-27 17:06:27 +0200

    refresh

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,10 +13,12 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -jump_force
 	
+	
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	
 	velocity.x = speed * horizontal_direction
 	
 	move_and_slide()
+	
 
 	print(velocity)
