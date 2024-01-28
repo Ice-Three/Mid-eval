1b6b7abaf0121e01f1232914c652261f92a29024
EuterNal <evgenii.nalpin@gmail.com>
2024-01-28 16:19:08 +0200

    Edited Oskarworld + added main menu

---
+++ scenes/CanvasLayer.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+func _on_level_1_button_down():
+	get_tree().change_scene_to_file("res://newsceneg/ollisworld.tscn")
+
+
+func _on_level_2_button_down():
+	get_tree().change_scene_to_file("res://IceThree/Oskarworld/Oskarworld.tscn")
